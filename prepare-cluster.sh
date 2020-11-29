412856da87ae63ddfa53e80221d92d71c664ca8c
Uriel Sepe <uriel.sepe@etermax.com>
2020-11-29 11:56:01 -0300

    add CRDs and change replicas place

--- prepare-cluster.sh
+++ prepare-cluster.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# KEDA CRDs
+kubectl apply -f https://raw.githubusercontent.com/kedacore/keda/main/config/crd/bases/keda.sh_scaledobjects.yaml
+kubectl apply -f https://raw.githubusercontent.com/kedacore/keda/main/config/crd/bases/keda.sh_triggerauthentications.yaml
+
+# ArgoCD
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
@@ -7,4 +12,5 @@ ARGO_PASS=`kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o
 
 echo "Argo Cluster password: $ARGO_PASS"
 
+# Apply Main App
 kubectl apply -f cluster.yml
